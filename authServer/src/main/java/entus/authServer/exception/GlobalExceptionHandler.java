41de25e96fcdc1474b1a79f1e8c3e22a83515bc9
KTH <taehyeonentus@gmail.com>
2025-08-02 23:27:19 +0900

    update global handler, invalidate session

--- authServer/src/main/java/entus/authServer/exception/GlobalExceptionHandler.java
+++ authServer/src/main/java/entus/authServer/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,10 +11,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
     @ExceptionHandler({InvalidTokenException.class, ExpiredJwtException.class, SignatureException.class})
     public String handleAuthExceptions(HttpServletRequest request, HttpServletResponse response) {
-        HttpSession session = request.getSession(false);
-        if (session != null)
-            session.invalidate();
-
         return "redirect:/login?error=token";
     }
 }
