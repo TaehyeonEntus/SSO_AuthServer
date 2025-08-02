c884195e268a241d9cfd390ee0910ed6240e1320
KTH <taehyeonentus@gmail.com>
2025-08-02 21:06:20 +0900

    Disable session

--- authServer/src/main/java/entus/authServer/config/SecurityConfig.java
+++ authServer/src/main/java/entus/authServer/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -23,6 +24,8 @@ public class SecurityConfig {
             throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((configurer) -> configurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/login", "/favicon.ico", "/error", "/register").permitAll()
                         .anyRequest().authenticated()
