06ad29ddb2cc0338dade11cdb52fdd8404142035
KTH <taehyeonentus@gmail.com>
2025-08-02 23:26:23 +0900

    update token send mechanism (to Cookie from Json)

--- authServer/src/main/java/entus/authServer/service/authorization/JwtHandler.java
+++ authServer/src/main/java/entus/authServer/service/authorization/JwtHandler.java
@@ -3,6 +3,7 @@ package entus.authServer.service.authorization;
 import entus.authServer.domain.user.User;
 import entus.authServer.domain.user.local.CustomUserDetails;
 import entus.authServer.repository.UserRepository;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,9 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
 /**
  * 로그인 시 JWT 발급 로직
  * 프레임워크 내부적 실행이라 명시적으로 json 작성
@@ -32,8 +36,20 @@ public class JwtHandler implements AuthenticationSuccessHandler {
         String accessToken = jwtGenerator.generateAccessToken(user);
         String refreshToken = jwtGenerator.generateRefreshToken(user);
 
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write("{\"accessToken\": \"" + accessToken + "\", \"refreshToken\": \"" + refreshToken + "\"}");
+        Cookie accessCookie = new Cookie("accessToken", accessToken);
+        accessCookie.setHttpOnly(true);
+        accessCookie.setPath("/");
+        response.addCookie(accessCookie);
+
+        Cookie refreshCookie = new Cookie("refreshToken", refreshToken);
+        refreshCookie.setHttpOnly(true);
+        refreshCookie.setPath("/");
+        response.addCookie(refreshCookie);
+
+        String redirectUrl = request.getParameter("redirectUrl");
+        if(redirectUrl != null)
+            response.sendRedirect(URLDecoder.decode(redirectUrl));
+        else
+            response.sendRedirect("http://localhost:8080/user");
     }
 }
