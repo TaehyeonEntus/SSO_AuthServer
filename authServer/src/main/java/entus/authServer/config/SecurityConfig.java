04c671e3f554b0df3d8e90880a01a08c0bbaa7d3
KTH <taehyeonentus@gmail.com>
2025-08-04 18:44:44 +0900

    add resource server domain to security system (cors)

--- authServer/src/main/java/entus/authServer/config/SecurityConfig.java
+++ authServer/src/main/java/entus/authServer/config/SecurityConfig.java
@@ -12,6 +12,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -23,11 +30,12 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
             throws Exception {
         http
+                .cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement((configurer) -> configurer
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/login", "/favicon.ico", "/error", "/register").permitAll()
+                        .requestMatchers("/login", "/favicon.ico", "/error", "/register", "/token/refresh").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
@@ -40,6 +48,18 @@ public class SecurityConfig {
                         .failureUrl("/login?error"));
 
         return http.build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:8080"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true); // ✅ 쿠키 전송 위해 true
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // 모든 경로에 적용
+        return source;
     }
 
     @Bean
