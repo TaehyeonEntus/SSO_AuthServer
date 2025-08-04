408469b38e510bbe49239a261cffc0db483d0bc0
KTH <taehyeonentus@gmail.com>
2025-08-04 18:43:50 +0900

    change token validation process to cookie from header

--- authServer/src/main/java/entus/authServer/controller/AuthController.java
+++ authServer/src/main/java/entus/authServer/controller/AuthController.java
@@ -1,10 +1,14 @@
 package entus.authServer.controller;
 
-import entus.authServer.domain.token.Token;
 import entus.authServer.domain.user.local.dto.LocalUserRegisterDTO;
 import entus.authServer.service.authentication.local.LocalUserService;
 import entus.authServer.service.authorization.JwtService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final LocalUserService localUserService;
     private final JwtService jwtService;
+
     @GetMapping("/register")
     public String register() {
         return "register";
@@ -25,7 +30,18 @@ public class AuthController {
     }
 
     @PostMapping("/token/refresh")
-    public Token refreshToken(@RequestBody String refreshToken){
-        return jwtService.reissue(refreshToken);
+    public ResponseEntity<?> refreshToken(HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies)
+            if (cookie != null)
+                if (cookie.getName().equals("refresh_token"))
+                    refreshToken = cookie.getValue();
+
+        if(refreshToken == null)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("refresh token null");
+
+        jwtService.reissue(request, response, refreshToken);
+        return ResponseEntity.ok().build();
     }
 }
