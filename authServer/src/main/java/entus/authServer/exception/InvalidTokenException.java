ad6fd76c77ac3751ec1a060cfffe1e87ca3f5fc1
KTH <taehyeonentus@gmail.com>
2025-08-09 19:37:55 +0900

    delete useless exception

--- authServer/src/main/java/entus/authServer/exception/InvalidTokenException.java
+++
@@ -1,9 +0,0 @@
-package entus.authServer.exception;
-
-public class InvalidTokenException extends RuntimeException{
-    public InvalidTokenException() {
-    }
-    public InvalidTokenException(String message) {
-        super(message);
-    }
-}
