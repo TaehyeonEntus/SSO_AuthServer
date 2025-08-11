f2f4b79a46eda2f5596b1e4a0ebf2b869b8f0a25
KTH <taehyeonentus@gmail.com>
2025-08-11 20:24:34 +0900

    change to JS rendering

--- authServer/src/main/java/entus/authServer/controller/HomeController.java
+++ authServer/src/main/java/entus/authServer/controller/HomeController.java
@@ -1,28 +1,33 @@
 package entus.authServer.controller;
 
+import entus.authServer.domain.dto.HomeResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
-    @GetMapping("/")
-    public String index() {
-        return "redirect:/home";
-    }
+    @Value("${resource.servers}")
+    private List<String> resourceServers;
 
     @GetMapping("/home")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @ResponseBody
+    @GetMapping("/api/home")
+    public HomeResponseDto apiHome() {
+        return new HomeResponseDto(resourceServers);
     }
 
     @GetMapping("/error")
     public String error() {
         return "error";
     }
-
 }
