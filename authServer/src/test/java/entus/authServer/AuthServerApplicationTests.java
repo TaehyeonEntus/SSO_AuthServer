adb81043f445f81125462193247054fc8bf12d49
KTH <taehyeonentus@gmail.com>
2025-09-11 22:26:03 +0900

    redis test

--- authServer/src/test/java/entus/authServer/AuthServerApplicationTests.java
+++ authServer/src/test/java/entus/authServer/AuthServerApplicationTests.java
@@ -2,15 +2,9 @@ package entus.authServer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class AuthServerApplicationTests {
-
-	@MockitoBean
-	private RedisTemplate<?, ?> redisTemplate;
-
 	@Test
 	void contextLoads() {
 	}
